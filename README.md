03efc284f596c5ebf13192ee8024fffc290f9299
paul wisehart <paul@oldcode.org>
2011-08-20 19:45:08 -0400

    2.3 works

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 - <http://developer.android.com/resources/samples/MultiResolution/res/layout-land/main.html>
 - <http://developer.android.com/guide/topics/resources/providing-resources.html>
 
+&nbsp;
 
-
+- <http://www.javahotchocolate.com/tutorials/android.html>
--- src/org/paul/sudoku/Sudoku.java
+++ src/org/paul/sudoku/Sudoku.java
@@ -3,6 +3,9 @@ package org.paul.sudoku;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -31,10 +34,7 @@ public class Sudoku extends Activity implements OnClickListener {
         
     }
     
-    
-
-
-
+    //@Override ????? 
     public void onClick(View v) {
     	switch (v.getId()) {
 
@@ -47,7 +47,27 @@ public class Sudoku extends Activity implements OnClickListener {
     	}
 
     }
+    
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case R.id.settings:
+    		startActivity(new Intent(this, Prefs.class));
+    		return true;
+    	}
 
+    	return false;
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.menu, menu);
+    	return true;
+    }
+    
+    
 }
 
